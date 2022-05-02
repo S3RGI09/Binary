5d9290711da88efad70b44fa5f8a48766db1dee9
Sergio Casero Verdial <96842235+S3RGI09@users.noreply.github.com>
2022-05-02 12:11:32 +0200

    Update Binary.py

--- Binary.py
+++ Binary.py
@@ -1,14 +1,16 @@
-print("┏┓ ╻┏┓╻┏━┓┏━┓╻ ╻")
-print("┣┻┓┃┃┗┫┣━┫┣┳┛┗┳┛")
-print("┗━┛╹╹ ╹╹ ╹╹┗╸ ╹")
-print("Creado por S3RGI09 (Sergio Casero Verdial)")
+#!/usr/bin/python3
+
+print("\033[1;31m"+"┏┓ ╻┏┓╻┏━┓┏━┓╻ ╻")
+print("\033[1;31m"+"┣┻┓┃┃┗┫┣━┫┣┳┛┗┳┛")
+print("\033[1;31m"+"┗━┛╹╹ ╹╹ ╹╹┗╸ ╹")
+
 def binario(decimal):
         binario = ""
         while decimal // 2 != 0:
                 binario = str(decimal % 2) + binario
                 decimal = decimal // 2
         return str(decimal) + binario
-print("-----------------------------------------------------------")
-numero = int(input("Introduce el numero que quieres convertir a binario: "))
+print("\033[1;34m"+"-----------------------------------------------------------")
+numero = int(input("\033[1;33m"+"Introduce el numero que quieres convertir a binario: "))
 print(binario(numero))
-print("-----------------------------------------------------------")
+print("\033[1;34m"+"-----------------------------------------------------------")
